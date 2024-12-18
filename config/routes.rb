cfbfbe0c104a1b28ace5394768daf1a939f467a1
Erika.K <koguchi.e915@gmail.com>
2024-12-18 17:37:19 +0900

    12/18 投稿(tasks)コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "top" => "homes#top"
+  root to: "homes#top"
+
+  # tasksコントローラのルーティングまとめて書く。
+  resources :tasks, only: [:new, :create, :index, :show, :edit, :update, :destroy]  
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'homes#about', as: 'about'
 end
