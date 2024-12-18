6b7484623fbd54b206ab35f13c8a62f76932f53a
Erika.K <koguchi.e915@gmail.com>
2024-12-18 16:49:58 +0900

    12/18 aboutページ作成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,8 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+// This file is automatically compiled by Webpack
+require('@rails/ujs').start();
+require('turbolinks').start();
+require('@rails/activestorage').start();
+require('channels');
