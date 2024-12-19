229c57759d198eafe32f3eee3542bc17c0cc2645
Erika.K <koguchi.e915@gmail.com>
2024-12-19 17:40:27 +0900

    12/19 サインアップにName追加・ログアウトとヘッダー未対応

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+
+  # deviseのコントローラは直接修正できないため、
+  # 全てのコントローラに対する処理を行える権限を持つ、ApplicationControllerに記述する必要があり。
+
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前に
+  # configure_permitted_parametersメソッドが実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # 名前の情報も保存できるようにする
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
