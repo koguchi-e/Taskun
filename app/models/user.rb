f3ece94a6f95ede8c1b7ab3981c8304027e89b84
ErikaK <koguchi.e915@gmail.com>
2024-12-21 09:06:31 +0900

    12/21 TaskとUserのアソシエーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :tasks, dependent: :destroy
+
   validates :name, presence: true
 
 end
