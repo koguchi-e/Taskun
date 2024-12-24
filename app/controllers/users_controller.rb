4a60ae772f85d0595c8685275c1e62daaef77912
Erika.K <koguchi.e915@gmail.com>
2024-12-24 15:15:44 +0900

    12/24 ユーザ編集画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,17 @@ class UsersController < ApplicationController
   end
 
   def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
   end
 
   def destroy
+  end
+
+  private
+  # ストロングパラメータ
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
