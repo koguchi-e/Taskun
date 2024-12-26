f041fb13e05c54030ef8cb55b99d01868f98f1c4
Erika.K <koguchi.e915@gmail.com>
2024-12-26 15:15:39 +0900

    12/26 devise.rb修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,6 +311,4 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
-  Devise.i18n_scope = :devise
-
 end
