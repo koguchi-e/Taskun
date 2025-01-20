46a52611523f45b28a968c280020b747e1fa57b4
Erika.K <koguchi.e915@gmail.com>
2025-01-20 13:20:02 +0000

    [Fix]テストデータ内容修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ lucas = User.find_or_create_by!(email: "lucas@example.com") do |user|
   user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
 end
 
-Task.find_or_create_by!(user_id: 1) do |task|
+Task.find_or_create_by!(user_id: 2) do |task|
   task.title = "テスト勉強三時間する"
   task.user = olivia
   task.keyword1 = "keyword1"
@@ -32,7 +32,7 @@ Task.find_or_create_by!(user_id: 1) do |task|
   task.keyword3 = "keyword3"
 end
 
-Task.find_or_create_by!(user_id: 2) do |task|
+Task.find_or_create_by!(user_id: 3) do |task|
   task.title = "洗濯をする"
   task.user = james
   task.keyword1 = "keyword1"
@@ -40,7 +40,7 @@ Task.find_or_create_by!(user_id: 2) do |task|
   task.keyword3 = "keyword3"
 end
 
-Task.find_or_create_by!(user_id: 3) do |task|
+Task.find_or_create_by!(user_id: 4) do |task|
   task.title = 'カリキュラム終わらせる'
   task.user = lucas
   task.keyword1 = "Ruby"
