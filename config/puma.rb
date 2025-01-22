d63b4216037c8f5fadc923ac424473d7d103f39d
Erika.K <koguchi.e915@gmail.com>
2025-01-22 13:55:09 +0000

    [Fix]config/puma.rb 再修正

--- config/puma.rb
+++ config/puma.rb
@@ -19,6 +19,9 @@ port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
+environment ENV.fetch("RAILS_ENV") { "development" }
+
+# Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
