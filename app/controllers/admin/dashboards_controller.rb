d6c73a2150017098e508a73b4fb101534bffba7f
Erika.K <koguchi.e915@gmail.com>
2025-01-24 15:22:36 +0900

    [Fix] 検索・入力したもの残す、入力してない時エラー

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -7,13 +7,17 @@ class Admin::DashboardsController < ApplicationController
   end
 
   def search
-    if params[:user_query].present?
-      @user_results = User.where("name LIKE ?", "%#{params[:user_query]}%")
+
+    @user_query = params[:user_query]
+    @task_query = params[:task_query]
+
+    if @user_query.blank? && @task_query.blank?
+      flash[:alert] = "検索条件を入力してください。"
     end
 
-    if params[:task_query].present?
-      @task_results = Task.where("title LIKE ?", "%#{params[:task_query]}%")
-    end
+    @user_results = User.where("name LIKE ?", "%#{@user_query}%") if @user_query.present?
+
+    @task_results = Task.where("title LIKE ?", "%#{@task_query}%") if @task_query.present?
 
     render :search
   end
