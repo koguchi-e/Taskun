fe052d50e64c47afb6625fd0ad6f60c435cc249b
Erika.K <koguchi.e915@gmail.com>
2025-01-28 16:08:08 +0900

    [Update] ユーザ詳細画面タスク名が長いときに日付と被らないよう変更、タスク名文字列制限追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,8 +3,8 @@ class Task < ApplicationRecord
   has_many :task_comments, dependent: :destroy
 
   # バリデーション
-  validates :title, presence: true
-  validates :keyword1, length: { maximum: 50 }, presence: true
-  validates :keyword2, length: { maximum: 50 }, presence: true
-  validates :keyword3, length: { maximum: 50 }, presence: true
+  validates :title, length: { maximum: 30 }, presence: true
+  validates :keyword1, length: { maximum: 30 }, presence: true
+  validates :keyword2, length: { maximum: 30 }, presence: true
+  validates :keyword3, length: { maximum: 30 }, presence: true
 end
