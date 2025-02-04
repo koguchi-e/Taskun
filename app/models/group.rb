e6bb4c99e77836a93e4f2b8591aca2af5762a679
Erika.K <koguchi.e915@gmail.com>
2025-02-04 11:32:14 +0900

    [Fix]group.image が nil になっているのを修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,6 +3,8 @@ class Group < ApplicationRecord
     has_many :group_memberships, dependent: :destroy
     has_many :members, through: :group_memberships, source: :user
 
+    has_one_attached :image
+
     validates :name, length: { maximum: 30 }, presence: true
     validates :summary, length: { maximum: 100 }
 end
