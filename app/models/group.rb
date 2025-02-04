bd5a4c07855763d35a06a3332d719ee47efb24bc
Erika.K <koguchi.e915@gmail.com>
2025-02-04 10:55:32 +0900

    [Update]GroupMembershipモデル作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,7 @@
 class Group < ApplicationRecord
     belongs_to :owner, class_name: "User"
-    has_many: group_memberships
-    has_many: members, through: :group_memberships, source: :user
-    has_one_attached :image
+    has_many :group_memberships, dependent: :destroy
+    has_many :members, through: :group_memberships, source: :user
 
     validates :name, length: { maximum: 30 }, presence: true
     validates :summary, length: { maximum: 100 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_02_03_055441) do
+ActiveRecord::Schema.define(version: 2025_02_04_015418) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,15 @@ ActiveRecord::Schema.define(version: 2025_02_03_055441) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "group_memberships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_group_memberships_on_group_id"
+    t.index ["user_id"], name: "index_group_memberships_on_user_id"
+  end
+
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.integer "owner_id"
@@ -103,5 +112,7 @@ ActiveRecord::Schema.define(version: 2025_02_03_055441) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "group_memberships", "groups"
+  add_foreign_key "group_memberships", "users"
   add_foreign_key "tasks", "users"
 end
