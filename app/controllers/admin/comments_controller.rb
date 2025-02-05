0ef62942805c6d09d986bf0be28ace4c9bcc5bc8
Erika.K <koguchi.e915@gmail.com>
2025-02-05 17:49:44 +0900

    [Upadate]タスク一覧・コメント一覧追加

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,7 +1,14 @@
 class Admin::CommentsController < ApplicationController
+  layout 'admin'
+  before_action :authenticate_admin!
+
   def index
+    @comments = TaskComment.page(params[:page])
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to admin_comments_path, notice: 'コメントを削除しました。'
   end
 end
