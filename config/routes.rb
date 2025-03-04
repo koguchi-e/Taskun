26d3181de17d8559da2c29c3713b5eb6a116cd45
Erika.K <koguchi.e915@gmail.com>
2025-03-04 13:37:09 +0900

    [Fix]新規登録ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    post "users/guest_sign_in", to: "public/sessions#guest_sign_in"
   end
 
   namespace :admin do
