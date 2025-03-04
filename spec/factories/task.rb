40d8152ee1a65c731d01105f79262e1f9ed635cb
Erika.K <koguchi.e915@gmail.com>
2025-03-04 16:40:46 +0900

    [Update]RSpecテスト・FactoryBot追加

---
+++ spec/factories/task.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+    factory :task do
+        title { Faker::Lorem.characters(number:10) }
+        keyword1 { Faker::Lorem.characters(number:10) }
+        keyword2 { Faker::Lorem.characters(number:10) }
+        keyword3 { Faker::Lorem.characters(number:10) }
+    end
+end
