71f042822c2acd4dc4967673aea58c3c1c50e916
Erika.K <koguchi.e915@gmail.com>
2025-03-10 13:20:40 +0900

    [Update]タスクコントローラテスト追加、タスク編集画面ボタン修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -37,28 +37,84 @@ describe "ゲストログインのテスト" do
     end
 end
 
-describe "投稿一覧画面のテスト" do
+describe "タスクのテスト" do
     let(:user){create(:user)}
     let!(:task) {create(:task,title:"カリキュラムを5ページ進める", keyword1:"Ruby",keyword2:"テスト",keyword1:"エンジニア")}
     before do
-        visit tasks_path
+        visit task_path
     end
-    context "一覧画面のテスト" do 
-        it "投稿されたものが表示されるかどうか" do
-            expect(page).to have_content task.title
-            expect(page).to have_link task.title
+    describe "投稿機能のテスト" do
+        context "表示の確認" do
+            it "投稿ボタンがあるか" do
+                expect(page).to have_selecter('button[type="submit-btn"]')
+            end
+        end
+        context "投稿処理のテスト" do
+            it "投稿後、詳細画面にリダイレクトする" do
+                fill_in "task[title]", with: Faker::Lorem.characters(number:10)
+                fill_in "task[keyword1]", with: Faker::Lorem.characters(number:10)
+                fill_in "task[keyword2]", with: Faker::Lorem.characters(number:10)
+                fill_in "task[keyword3]", with: Faker::Lorem.characters(number:10)
+                click_button "投稿"
+                expect(page).to have_current_path task_path(Task.last)
+            end
         end
     end
-    context "新規投稿のテスト" do
-        it "投稿ボタンがあるか" do
-            expect(page).to have_selecter('button[type="submit-btn"]')
+    
+    describe "一覧画面のテスト" do
+        context "表示の確認" do 
+            it "投稿されたものが表示されるかどうか" do
+                expect(page).to have_content task.title
+                expect(page).to have_link task.title
+            end
         end
     end
-    context "投稿できるか" do
-        it "投稿後、詳細画面にリダイレクトする" do
-            fill_in "task[title]"
+    
+    describe "詳細画面のテスト" do
+        before do 
+            visit task_path(task)
+        end
+        context "表示の確認" do
+            it "削除リンクが存在するか" do
+                expect(page).to  have_selector("a[href='/tasks/#{task.id}][data-method='delete']")
+            end
+            it "編集リンクが存在するか" do
+                expect(page).to have_selector("a[href='/tasks/#{task.id}/edit']")
+            end
         end
     end
+
+    describe "編集画面のテスト" do
+        let!(:user){create(:user)}
+        let!(:task){create(:task,title:"カリキュラムを5ページ進める", keyword1:"Ruby",keyword2:"テスト",keyword1:"エンジニア")}
+        context "表示の確認" do
+            before do
+                sign_in user
+                visit edit_task_path(task)
+            end
+            it "入力欄に編集前のタイトルとキーワードがフォームに表示(セット)されている" do
+                expect(page).to have_field "task[title]", with: task.title
+                expect(page).to  have_field "task[keyword1]", with: task.keyword1
+                expect(page).to  have_field "task[keyword2]", with: task.keyword2
+                expect(page).to  have_field "task[keyword3]", with: task.keyword3
+            end
+            it "保存ボタンが表示される " do
+                expect(page).to have_button "保存"
+            end
+        end
+        context "更新処理に関するテスト" do
+            it "更新後リダイレクト先は正しいか" do
+                fill_in "task[title]", with: Faker::Lorem.characters(number:10)
+                fill_in "task[keyword1]", with: Faker::Lorem.characters(number:10)
+                fill_in "task[keyword2]", with: Faker::Lorem.characters(number:10)
+                fill_in "task[keyword3]", with: Faker::Lorem.characters(number:10)
+                click_button "保存"
+                expect(task).to have_current_path task_path(task)
+            end
+        end
+    end
+    
+    
 end
 
 
