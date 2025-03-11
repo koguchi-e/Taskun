eb34a2115747395e92be4349c1df0ab0e72c6c6e
Erika.K <koguchi.e915@gmail.com>
2025-03-12 08:32:45 +0900

    [Fix]ゲストログインのリダイレクト先変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -15,7 +15,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to user_path(user), notice: "ゲストユーザーでログインしました。"
+    redirect_to users_path, notice: "ゲストユーザーでログインしました。"
   end
 
   # GET /resource/sign_in
--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -4,15 +4,13 @@ require 'rails_helper'
 
 RSpec.describe "ログイン前のテスト" do
     let(:user){create(:user)}
-
     before do
         visit new_user_session_path
         fill_in "user_email", with: user.email
         fill_in "user_password", with: user.password
-        click_button "login-btn"
+        click_button "ログイン"
         visit root_path
     end
-
     context "表示の確認" do
         it "root_pathが/であるか" do
             expect(current_path).to eq("/")
@@ -26,7 +24,7 @@ describe "ゲストログインのテスト" do
     end
     context "ゲストログインボタンの表示" do
         it "ログインボタンが表示されているかどうか" do
-            expect(page).to have_link "ゲストログイン", href: "users/guest_sign_in"
+            expect(page).to have_link "ゲストログイン（閲覧用）", href: "users/guest_sign_in"
         end
     end
     context "ゲストログインの動作確認" do
@@ -35,4 +33,25 @@ describe "ゲストログインのテスト" do
             expect(pave).to have_link users_path
         end
     end
+end
+
+describe "ユーザーのテスト" do
+  let(:user) { FactoryBot.create(:user) }
+  let!(:task) { FactoryBot.create(:task, user: user, title: "カリキュラムを5ページ進める", keyword1: "Ruby", keyword2: "テスト", keyword3: "エンジニア") }
+
+  before do
+    sign_in user
+    visit users_path
+  end
+
+  describe "一覧画面のテスト" do
+    let(:user) { FactoryBot.create(:user) }
+
+    context "表示の確認" do
+      it "ユーザーが表示されるかどうか" do
+        expect(page).to have_content user.name
+        expect(page).to have_link user.name
+      end
+    end
+  end
 end
