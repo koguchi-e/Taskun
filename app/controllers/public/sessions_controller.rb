b529ebdd45d2df04c4ddcb6d431318416eb0f979
Erika.K <koguchi.e915@gmail.com>
2025-03-12 08:36:14 +0900

    [Fix]ゲストログインのリダイレクト先変更

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -15,7 +15,7 @@ class Public::SessionsController < Devise::SessionsController
   def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to users_path, notice: "ゲストユーザーでログインしました。"
+    redirect_to users_path(user), notice: "ゲストユーザーでログインしました。"
   end
 
   # GET /resource/sign_in
