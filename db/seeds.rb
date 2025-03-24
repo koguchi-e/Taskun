5dd5e62de9d955ebe942c8a7e686b3150d80ac47
Erika.K <koguchi.e915@gmail.com>
2025-03-24 12:50:27 +0900

    [Fix]seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-test_tarou = User.find_or_create_by!(email: "aaa@gmail.com") do |user|
+test-tarou = User.find_or_create_by!(email: "test-tarou@test.com") do |user|
   user.name = "テスト太郎"
   user.password = "123456"
   user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/test-tarou.jpg"), filename: "test-tarou.jpg")
 end
 
-yamada = User.find_or_create_by!(email: "yamada@example.com") do |user|
+yamada = User.find_or_create_by!(email: "yamada@test.com") do |user|
   user.name = "山田"
   user.password = "123456"
   user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename: "sample-user1.jpg")
 end
 
-tanaka = User.find_or_create_by!(email: "tanaka@example.com") do |user|
+tanaka = User.find_or_create_by!(email: "tanaka@test.com") do |user|
   user.name = "田中"
   user.password = "123456"
   user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename: "sample-user2.jpg")
 end
 
-satou = User.find_or_create_by!(email: "satou@example.com") do |user|
+satou = User.find_or_create_by!(email: "satou@test.com") do |user|
   user.name = "佐藤"
   user.password = "123456"
   user.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename: "sample-user3.jpg")
 end
 
-Task.find_or_create_by!(user: test_tarou, title: "Rubyの勉強する") do |task|
+Task.find_or_create_by!(user: satou, title: "Rubyの勉強する") do |task|
   task.keyword1 = "テスト"
   task.keyword2 = "Ruby"
   task.keyword3 = "勉強"
@@ -54,13 +54,13 @@ Task.find_or_create_by!(user: satou, title: "カリキュラム終わらせる")
   task.keyword3 = "勉強"
 end
 
-Task.find_or_create_by!(user: test_tarou, title: "フロントの勉強する") do |task|
+Task.find_or_create_by!(user: satou, title: "フロントの勉強する") do |task|
   task.keyword1 = "テスト"
   task.keyword2 = "Ruby"
   task.keyword3 = "勉強"
 end
 
-Task.find_or_create_by!(user: test_tarou, title: "GitHubの勉強する") do |task|
+Task.find_or_create_by!(user: satou, title: "GitHubの勉強する") do |task|
   task.keyword1 = "テスト"
   task.keyword2 = "Ruby"
   task.keyword3 = "勉強"
