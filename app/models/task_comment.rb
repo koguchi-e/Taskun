5fd6dc6c212d23385098f9ef84bc5dcb36fdb4a4
Erika.K <koguchi.e915@gmail.com>
2025-03-25 09:50:09 +0900

    [Fix]コメント・フォローの通知が出るように修正

--- app/models/task_comment.rb
+++ app/models/task_comment.rb
@@ -3,4 +3,8 @@ class TaskComment < ApplicationRecord
   belongs_to :task
 
   has_many :notifications, as: :notifiable, dependent: :destroy
+
+  after_create do
+    notifications.create(user_id: task.user_id, read: false)
+  end
 end
