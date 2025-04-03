f3c9ad177f4e543175e369d6c01c2742abdc413e
Erika.K <koguchi.e915@gmail.com>
2025-04-03 15:42:14 +0900

    [Update]いいね機能・local

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,21 @@
 class Public::FavoritesController < ApplicationController
   def create
-    task = Task.find(params[:task_id])
-    favorite = current_user.favorites.new(task_id: task.id)
+    @task = Task.find(params[:task_id])
+    favorite = current_user.favorites.new(task_id: @task.id)
     favorite.save
-    redirect_to task_path(task)
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
-    task = Task.find(params[:task_id])
-    favorite = current_user.favorites.find_by(task_id: task.id)
+    @task = Task.find(params[:task_id])
+    favorite = current_user.favorites.find_by(task_id: @task.id)
     favorite.destroy
-    redirect_to task_path(task)
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
