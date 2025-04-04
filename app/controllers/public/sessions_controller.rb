8c34693dd5adf49705953b0c7b7e573fd1ed3686
Erika.K <koguchi.e915@gmail.com>
2025-04-04 16:31:09 +0900

    [Fix]退会機能・user_stateメソッド誤字修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -42,9 +42,9 @@ class Public::SessionsController < Devise::SessionsController
 
   private
     def user_state
-      user = User.find_by(email: params[:custmer][:email])
+      user = User.find_by(email: params[:user][:email])
       return if user.nil?
-      return unless usr.valid_password?(params[:customer][:password])
+      return unless user.valid_password?(params[:user][:password])
 
       unless user.is_active
         redirect_to new_user_registration_path, alert: "退会済みのアカウントです。再度ご登録ください。"
