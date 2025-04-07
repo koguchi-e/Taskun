8c1a2a68bce5e4fc2499bf3d82edc098df0fe868
Erika.K <koguchi.e915@gmail.com>
2025-04-07 13:55:23 +0000

    [fix]ログイン前画面ハンバーガーメニュー修正

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2025_04_07_021307) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2025_04_07_021307) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -111,8 +111,6 @@ ActiveRecord::Schema.define(version: 2025_04_07_021307) do
     t.string "keyword2"
     t.string "keyword3"
     t.integer "user_id", null: false
-    t.boolean "completed", default: false, null: false
-    t.datetime "completed_at"
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
