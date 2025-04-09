359316e3c24473308986c7e0d3419cecadfd899f
Erika.K <koguchi.e915@gmail.com>
2025-04-09 17:23:57 +0900

    [Fix]before_action:is_matching_login_user追加・投稿者本人以外変更不可

--- app/controllers/public/tasks_controller.rb
+++ app/controllers/public/tasks_controller.rb
@@ -1,5 +1,6 @@
 ﻿class Public::TasksController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def new
     @task = Task.new
