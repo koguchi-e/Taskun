9a34f9047bd5305ba37a219c1a73e1e195e78340
Erika.K <koguchi.e915@gmail.com>
2025-04-22 15:15:39 +0900

    [Fix]管理者画面のN+1問題を解消

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,7 +3,7 @@ class Admin::DashboardsController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.page(params[:page])
+    @users = User.includes(image_attachment: :blob).page(params[:page])
   end
 
   def search
@@ -14,10 +14,24 @@ class Admin::DashboardsController < ApplicationController
     if @user_query.blank? && @task_query.blank? && @comment_query.blank?
       flash[:alert] = "検索条件を入力してください。"
     else
-      @user_results = User.where("name LIKE ?", "%#{@user_query}%") if @user_query.present?
-      @task_results = Task.where("title LIKE ?", "%#{@task_query}%") if @task_query.present?
-      @comment_results = TaskComment
-      .where("comment LIKE ?", "%#{@comment_query}%") if @comment_query.present?
+      if @user_query.present?
+        @user_results = User
+          .includes(image_attachment: :blob)
+          .where("name LIKE ?", "%#{@user_query}%")
+      end
+
+      if @task_query.present?
+        @task_results = Task
+          .includes(:user => { image_attachment: :blob }, comments: [])
+          .where("title LIKE ?", "%#{@task_query}%")
+      end
+
+      if @comment_query.present?
+        @comment_results = TaskComment
+          .includes(:task, user: { image_attachment: :blob })
+          .where("comment LIKE ?", "%#{@comment_query}%")
+      end
+
       render :search
     end
   end
